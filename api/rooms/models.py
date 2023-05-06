7f54de7d418b86485df26a82235d5fa4b064727e
zoe <ool2o8@github.com>
2023-05-06 16:40:20 +0900

    FEAT 회의실 이미지 업로드

--- api/rooms/models.py
+++ api/rooms/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 from users.models import User
 from django.utils.translation import gettext_lazy as _
+
+
 class RoomImages(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    image=models.ImageField(upload_to="images/", db_column="image")
+    id = models.BigAutoField(primary_key=True, auto_created=True)
+    image = models.ImageField(upload_to="images/", db_column="image")
 
 
 class Room(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name=models.CharField(blank=False, max_length=100, null=False)
-    discription=models.TextField()
+    name = models.CharField(blank=False, max_length=100, null=False)
+    discription = models.TextField()
     images = models.ForeignKey(RoomImages, on_delete=models.CASCADE, null=True)
+
 
 class Reservation(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, related_name="reservation", on_delete=models.CASCADE, db_column="user_id")
+    user = models.ForeignKey(
+        User, related_name="reservation", on_delete=models.CASCADE, db_column="user_id"
+    )
     start_at = models.DateTimeField()
     end_at = models.DateTimeField()
-    
+
     class ReservationStatus(models.TextChoices):
-        NRESERVED = 'NRES', _('NotReserved')
-        RESERVED = 'RES', _('Reserved')
-        BLOCKED = 'BLK', _('Blocked')
+        NRESERVED = "NRES", _("NotReserved")
+        RESERVED = "RES", _("Reserved")
+        BLOCKED = "BLK", _("Blocked")
 
     reservation_status = models.CharField(
         max_length=16,
@@ -30,12 +35,9 @@ class Reservation(models.Model):
     )
 
 
-
 class DailyReservationCard(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField(auto_now_add=True)
-    room = models.ForeignKey(Room, related_name="reservation", on_delete=models.CASCADE, db_column="room_id")
-    
-
-
-
+    room = models.ForeignKey(
+        Room, related_name="reservation", on_delete=models.CASCADE, db_column="room_id"
+    )
